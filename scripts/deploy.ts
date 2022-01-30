f1318d261570cdf14cd2c871fb7fca01e3d4adb5
cocodigrande2021 <cocodigrande@protonmail.com>
2022-01-30 18:15:33 +0100

    Generate deployment output conditionally

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,8 +1,4 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
+import * as dotenv from "dotenv";
 import { ethers } from "hardhat";
 // eslint-disable-next-line node/no-missing-import
 import { CryptoCocks } from "../typechain";
@@ -13,6 +9,7 @@ import {
   saveOwnerJson,
   // eslint-disable-next-line node/no-missing-import
 } from "./generator";
+dotenv.config();
 
 async function main() {
   // deploy libraries
@@ -33,17 +30,21 @@ async function main() {
 
   console.log("CryptoCocks contract deployed to:", cryptoCocks.address);
 
-  const signers = await ethers.getSigners();
-  const owner = signers[0];
-  const network = await ethers.getDefaultProvider().getNetwork();
-  const chainId = network.chainId;
+  const generate = process.env.GENERATE === "true";
 
-  saveOwnerJson(owner.address);
-  saveChainJson(chainId);
-  saveAddressesJson({
-    CryptoCocks: cryptoCocks.address,
-  });
-  saveABIJson("CryptoCocks");
+  if (generate) {
+    const signers = await ethers.getSigners();
+    const owner = signers[0];
+    const network = await ethers.getDefaultProvider().getNetwork();
+    const chainId = network.chainId;
+
+    saveOwnerJson(owner.address);
+    saveChainJson(chainId);
+    saveAddressesJson({
+      CryptoCocks: cryptoCocks.address,
+    });
+    saveABIJson("CryptoCocks");
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
