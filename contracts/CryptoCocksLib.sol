f4fb4b20f4f714e5335c7c414cadffa37ec18986
cocodigrande2021 <cocodigrande@protonmail.com>
2022-02-03 19:12:37 +0100

    Add final cids

--- contracts/CryptoCocksLib.sol
+++ contracts/CryptoCocksLib.sol
@@ -2,20 +2,19 @@
 pragma solidity 0.8.7;
 
 library CryptoCocksLib {
-
     function getCid(uint id) external pure returns (string memory cid) {
         string memory batch;
 
         if (id <= 2000) {
-            batch = "b1";
+            batch = "bafybeiesbbihtfdj3kqbah5642p7drsb6hrzwzksezbgb2t2ojjwgh2k5m";
         } else if (id <= 4000) {
-            batch = "b2";
+            batch = "bafybeifclnruolpdcsouhmzhnardvpzroxk6qouc53drw4vh2f3zdoouya";
         } else if (id <= 6000) {
-            batch = "b3";
+            batch = "bafybeihbeszvaoc3exx6ji77g74nyuqmoz2scdykudna3qd6xzgygn36ra";
         } else if (id <= 8000) {
-            batch = "b4";
+            batch = "bafybeidl3uswhq65hnfvgj6bfahbvdb57y7cxiaelgct6q7raweubcms6u";
         } else {
-            batch = "b5";
+            batch = "bafybeifx2hrh6mhbpcivo4z53l76uqwc6fth4nf4qah6aow7e62lcka3d4";
         }
 
         return string(abi.encodePacked("ipfs://", batch, "/"));
