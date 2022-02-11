6309e5db0fc83c5bf1ae6479b909d1953673f395
cocodigrande2021 <cocodigrande@protonmail.com>
2022-02-11 18:12:28 +0100

    Set gasPrice

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -48,6 +48,7 @@ const config: HardhatUserConfig = {
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
+    gasPrice: 70,
     coinmarketcap: process.env.COINMARKETCAP,
   },
   etherscan: {
